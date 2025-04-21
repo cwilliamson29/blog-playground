c43a75d3107f28af684d7dd83a0dd76d808592d0
Chris Williamson <chriswill0629@gmail.com>
2025-04-20 23:32:57 -0400

    todos mutating data with react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-//import ToDoList from "./react-query/ToDoList.tsx";
 import Posts from "./react-query/Posts.tsx";
 import TodoForm from "./react-query/TodoForm.tsx";
 import ToDoList from "./react-query/ToDoList.tsx";
